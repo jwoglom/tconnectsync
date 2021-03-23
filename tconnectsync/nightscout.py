89b40dcecb3bb6bd5bf0756edbe7906ee3525f92
James Woglom <j@wogloms.net>
2021-03-22 22:37:32 -0400

    nightscout: use urljoin so trailing backslash is not required in URL. add api status endpoint

--- tconnectsync/nightscout.py
+++ tconnectsync/nightscout.py
@@ -4,6 +4,8 @@ import hashlib
 import time
 import urllib.parse
 
+from urllib.parse import urljoin
+
 from .parser.nightscout import ENTERED_BY
 
 try:
@@ -14,7 +16,7 @@ except Exception:
 
 
 def upload_nightscout(ns_format, entity='treatments'):
-	upload = requests.post(NS_URL + 'api/v1/' + entity + '?api_secret=' + NS_SECRET, json=ns_format, headers={
+	upload = requests.post(urljoin(NS_URL, 'api/v1/' + entity + '?api_secret=' + NS_SECRET), json=ns_format, headers={
 		'Accept': 'application/json',
 		'Content-Type': 'application/json',
 		'api-secret': hashlib.sha1(NS_SECRET.encode()).hexdigest()
@@ -22,7 +24,7 @@ def upload_nightscout(ns_format, entity='treatments'):
 	print("Nightscout upload status:", upload.status_code, upload.text)
 
 def delete_nightscout(entity):
-	upload = requests.delete(NS_URL + 'api/v1/' + entity + '?api_secret=' + NS_SECRET, json={}, headers={
+	upload = requests.delete(urljoin(NS_URL, 'api/v1/' + entity + '?api_secret=' + NS_SECRET), json={}, headers={
 		'Accept': 'application/json',
 		'Content-Type': 'application/json',
 		'api-secret': hashlib.sha1(NS_SECRET.encode()).hexdigest()
@@ -30,7 +32,7 @@ def delete_nightscout(entity):
 	print("Nightscout delete status:", upload.status_code, upload.text)
 
 def put_nightscout(ns_format, entity):
-	upload = requests.put(NS_URL + 'api/v1/' + entity + '?api_secret=' + NS_SECRET, json=ns_format, headers={
+	upload = requests.put(urljoin(NS_URL, 'api/v1/' + entity + '?api_secret=' + NS_SECRET), json=ns_format, headers={
 		'Accept': 'application/json',
 		'Content-Type': 'application/json',
 		'api-secret': hashlib.sha1(NS_SECRET.encode()).hexdigest()
@@ -38,7 +40,7 @@ def put_nightscout(ns_format, entity):
 	print("Nightscout put status:", upload.status_code, upload.text)
 
 def last_uploaded_nightscout_entry(eventType):
-    latest = requests.get(NS_URL + 'api/v1/treatments?count=1&find[enteredBy]=' + urllib.parse.quote(ENTERED_BY) + '&find[eventType]=' + urllib.parse.quote(eventType) + '&ts=' + str(time.time()), headers={
+    latest = requests.get(urljoin(NS_URL, 'api/v1/treatments?count=1&find[enteredBy]=' + urllib.parse.quote(ENTERED_BY) + '&find[eventType]=' + urllib.parse.quote(eventType) + '&ts=' + str(time.time())), headers={
 		'api-secret': hashlib.sha1(NS_SECRET.encode()).hexdigest()
     })
     j = latest.json()
@@ -47,10 +49,21 @@ def last_uploaded_nightscout_entry(eventType):
     return None
 
 def last_uploaded_nightscout_activity(activityType):
-    latest = requests.get(NS_URL + 'api/v1/activity?find[enteredBy]=' + urllib.parse.quote(ENTERED_BY) + '&find[activityType]=' + urllib.parse.quote(activityType) + '&ts=' + str(time.time()), headers={
+    latest = requests.get(urljoin(NS_URL, 'api/v1/activity?find[enteredBy]=' + urllib.parse.quote(ENTERED_BY) + '&find[activityType]=' + urllib.parse.quote(activityType) + '&ts=' + str(time.time())), headers={
 		'api-secret': hashlib.sha1(NS_SECRET.encode()).hexdigest()
     })
     j = latest.json()
     if j and len(j) > 0:
         return j[0]
     return None
+
+"""
+Returns general status information about the Nightscout server.
+"""
+def api_status():
+	status = requests.get(urljoin(NS_URL, 'api/v1/status.json'), headers={
+		'api-secret': hashlib.sha1(NS_SECRET.encode()).hexdigest()
+    })
+	if status.status_code != 200:
+		raise Exception('HTTP error status code (%d) from Nightscout: %s' % (status.status_code, status.text))
+	return status.json()
