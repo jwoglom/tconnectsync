093a7f062706471f17c8621949cd8c5e6ee9ef86
James Woglom <j@wogloms.net>
2021-04-21 01:27:13 -0400

    tests: add bolus tests

--- tests/test_process.py
+++ tests/test_process.py
@@ -10,6 +10,7 @@ from tconnectsync.parser.nightscout import NightscoutEntry
 from .api.fake import TConnectApi
 from .nightscout_fake import NightscoutApi
 from .sync.test_basal import TestBasalSync
+from .sync.test_bolus import TestBolusSync
 
 class TestProcessTimeRange(unittest.TestCase):
     maxDiff = None
@@ -55,7 +56,7 @@ class TestProcessTimeRange(unittest.TestCase):
         process_time_range(tconnect, nightscout, start, end, pretend=False)
 
         self.assertEqual(len(nightscout.uploaded_entries["treatments"]), 4)
-        self.assertDictEqual(nightscout.uploaded_entries, {
+        self.assertDictEqual(dict(nightscout.uploaded_entries), {
             "treatments": [
                 NightscoutEntry.basal(0.8, 20.35, "2021-03-16 00:00:00-04:00", reason="tempDelivery"),
                 NightscoutEntry.basal(0.799, 5.0, "2021-03-16 00:20:21-04:00", reason="profileDelivery"),
@@ -97,7 +98,7 @@ class TestProcessTimeRange(unittest.TestCase):
         process_time_range(tconnect, nightscout, start, end, pretend=False)
 
         self.assertEqual(len(nightscout.uploaded_entries["treatments"]), 2)
-        self.assertDictEqual(nightscout.uploaded_entries, {
+        self.assertDictEqual(dict(nightscout.uploaded_entries), {
             "treatments": [
                 NightscoutEntry.basal(0.797, 5.0, "2021-03-16 00:25:21-04:00", reason="algorithmDelivery"),
                 NightscoutEntry.basal(0, 2693/60, "2021-03-16 00:30:21-04:00", reason="algorithmDelivery")
@@ -146,7 +147,7 @@ class TestProcessTimeRange(unittest.TestCase):
                 NightscoutEntry.basal(0, 2693/60, "2021-03-16 00:30:21-04:00", reason="algorithmDelivery")
         ]})
         self.assertEqual(len(nightscout.put_entries["treatments"]), 1)
-        self.assertDictEqual(nightscout.put_entries, {
+        self.assertDictEqual(dict(nightscout.put_entries), {
             "treatments": [
                 {
                     "_id": "nightscout_id",
@@ -154,6 +155,38 @@ class TestProcessTimeRange(unittest.TestCase):
                 }
             ]
         })
+        self.assertDictEqual(nightscout.deleted_entries, {})
+
+    """No data in Nightscout. Uploads all bolus data from tconnect."""
+    def test_new_ciq_bolus_data(self):
+        tconnect = TConnectApi()
+
+        start = datetime.datetime(2021, 4, 20, 12, 0)
+        end = datetime.datetime(2021, 4, 21, 12, 0)
+
+        tconnect.controliq.therapy_timeline = self.stub_therapy_timeline
+
+        def fake_therapy_timeline_csv(time_start, time_end):
+            return TestBolusSync.get_example_csv_bolus_events()
+
+        tconnect.ws2.therapy_timeline_csv = fake_therapy_timeline_csv
+
+        nightscout = NightscoutApi()
+
+        nightscout.last_uploaded_entry = self.stub_last_uploaded_entry
+        nightscout.last_uploaded_activity = self.stub_last_uploaded_activity
+
+        process_time_range(tconnect, nightscout, start, end, pretend=False)
+
+        pprint.pprint(nightscout.uploaded_entries)
+        self.assertEqual(len(nightscout.uploaded_entries["treatments"]), 3)
+        self.assertDictEqual(dict(nightscout.uploaded_entries), {
+            "treatments": [
+                NightscoutEntry.bolus(13.53, 75, "2021-04-01 12:58:26-04:00", notes="Standard/Correction"),
+                NightscoutEntry.bolus(1.25, 0, "2021-04-01 23:23:17-04:00", notes="Standard (Override)"),
+                NightscoutEntry.bolus(1.7, 0, "2021-04-02 01:00:47-04:00", notes="Automatic Bolus/Correction"),
+        ]})
+        self.assertDictEqual(nightscout.put_entries, {})
         self.assertDictEqual(nightscout.deleted_entries, {})
 
 
