7d813ee8fbd48f5c8fd35a807ff025dbf218f475
James Woglom <j@wogloms.net>
2021-07-09 21:49:34 -0400

    main: update logging config to show timestamps

--- main.py
+++ main.py
@@ -40,10 +40,16 @@ def main():
     args = parse_args()
 
     if args.verbose:
-        logging.basicConfig(level=logging.DEBUG)
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format='%(asctime)s %(levelname)-8s %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S')
         logging.root.debug("Set logging level to DEBUG")
     else:
-        logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(
+            level=logging.INFO,
+            format='%(asctime)s %(levelname)-8s %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S')
 
     if args.auto_update and (args.start_date or args.end_date):
         raise Exception('Auto-update cannot be used with start/end date')
