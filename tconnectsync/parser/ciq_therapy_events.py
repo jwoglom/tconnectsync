15951cf68e77d094eb8988804a59d2e1886d7550
Jarred Yaw <jarred.yaw@gmail.com>
2022-08-22 20:15:49 -0500

    fix unknown therapy event error in ciq_therapy_event.py

--- tconnectsync/parser/ciq_therapy_events.py
+++ tconnectsync/parser/ciq_therapy_events.py
@@ -1,4 +1,4 @@
-from tconnectsync.domain.therapy_event import BolusTherapyEvent, CGMTherapyEvent
+from tconnectsync.domain.therapy_event import BolusTherapyEvent, CGMTherapyEvent, BGTherapyEvent
 from tconnectsync.parser.tconnect import TConnectEntry
 
 import logging
@@ -8,12 +8,16 @@ logger = logging.getLogger(__name__)
 def split_therapy_events(ciqTherapyEvents):
     bolusEvents = []
     cgmEvents = []
+    bgEvents = []
     for e in ciqTherapyEvents['event']:
         event = TConnectEntry.parse_therapy_event(e)
-        if type(event) == BolusTherapyEvent:
+        if isinstance(event, BolusTherapyEvent):
             bolusEvents.append(event)
-        elif type(event) == CGMTherapyEvent:
+        elif isinstance(event, CGMTherapyEvent): 
             cgmEvents.append(event)
+        elif isinstance(event, BGTherapyEvent):
+            bgEvents.append(event)
         
-    logger.debug("split_therapy_events: %d bolus, %d CGM" % (len(bolusEvents), len(cgmEvents)))
+    logger.debug("split_therapy_events: %d bolus, %d CGM, %d BG" % (len(bolusEvents), len(cgmEvents), len(bgEvents)))
+    # TODO: BG events (CGM Calibration) values are not currently returned from ciq_therapy_events.py
     return bolusEvents, cgmEvents
