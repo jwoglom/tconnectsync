04643255fe0a66e749f864a87329d4635b24195a
James Woglom <j@wogloms.net>
2024-10-02 01:34:47 -0400

    heroku_helpers

---
+++ tconnectsync/sync/tandemsource/heroku_helpers.py
@@ -0,0 +1,17 @@
+from ...features import DEFAULT_FEATURES
+from .choose_device import TandemSourceChooseDevice
+from .process import ProcessTimeRange
+from ... import secret
+
+import datetime
+
+def run_oneshot(tconnect, nightscout, pretend=False, features=DEFAULT_FEATURES, secret_arg=None, time_start=None, time_end=None):
+    if not time_start and not time_end:
+        time_end = datetime.datetime.now()
+        time_start = time_end - datetime.timedelta(days=1)
+
+    if not secret_arg:
+        secret_arg = secret
+
+    tconnectDevice = TandemSourceChooseDevice(secret_arg, tconnect).choose()
+    return ProcessTimeRange(tconnect, nightscout, tconnectDevice, pretend, features).process(time_start, time_end)
