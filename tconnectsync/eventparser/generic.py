98b7edc904e78f4bf58a514248978182a1102d7e
James Woglom <j@wogloms.net>
2024-12-10 17:44:54 -0500

    eventparser: handle unknown ids in Event()

--- tconnectsync/eventparser/generic.py
+++ tconnectsync/eventparser/generic.py
@@ -8,7 +8,13 @@ from .events import EVENT_IDS
 from .utils import batched
 
 
-Event = lambda x: EVENT_IDS[RawEvent.build(x).id].build(x)
+def Event(x):
+    raw_event = RawEvent.build(x)
+    if not raw_event.id in EVENT_IDS:
+        return raw_event
+
+    return EVENT_IDS[raw_event.id].build(x)
+
 Events = lambda x: (Event(bytearray(e)) for e in batched(x, EVENT_LEN))
 
 def decode_raw_events(raw):
